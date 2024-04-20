e0d080ac354a6e912267ecdc47f558cccafe84c5
Mohammad ali <m.hashemipourr@gmail.com>
2024-04-20 21:11:48 +0330

    wpm counter added

--- typingTest.py
+++ typingTest.py
@@ -13,27 +13,46 @@ stdscr.keypad(True)
 curses.init_pair(1,curses.COLOR_RED,curses.COLOR_BLACK)
 curses.init_pair(2,curses.COLOR_WHITE,curses.COLOR_BLACK)
 
-str = stringGen.stringGenerator(30)
+stdscr.addstr("WPM: -\n")
 
-stdscr.addstr(str)
+testString = stringGen.stringGenerator(10)
+
+stdscr.addstr(testString)
 i = 0
-stdscr.move(0,0)
+stdscr.move(1,0)
+global words;
+words = 0
+correctLetters = 0
+wrongLetter = 0
+startTime = 0
 
-while i < str.__len__():
+while i < testString.__len__():
+    
     ch = stdscr.getch()
-    if ch == ord(str[i]):
-        stdscr.addch(0,i,ch)
+
+    if ch == curses.ascii.SP:
+        correctLetters += 1
+        wpm = int(correctLetters / (time.time() - startTime) * 12)
+        stdscr.move(0,5)
+        stdscr.addstr(0,5,str(wpm))
+        stdscr.addch(1,i,testString[i],curses.color_pair(1))
+        words += 1
+        i += 1
+        stdscr.refresh()
+    elif ch == ord(testString[i]):
+        stdscr.addch(1,i,ch)
+        correctLetters += 1
+        if (i == 0):
+            startTime = time.time()
         i += 1
         stdscr.refresh()
     elif ch == curses.KEY_BACKSPACE:
-        stdscr.addch(0,i-1,str[i-1])
+        stdscr.addch(1,i-1,testString[i-1])
         i -= 1
-        stdscr.move(0,i)
-    elif ch == curses.ascii.SP:
-        stdscr.addch(0,i,str[i],curses.color_pair(1))
-        i += 1
+        stdscr.move(1,i)
     else:
-        stdscr.addch(0,i,ch,curses.color_pair(1))
+        stdscr.addch(1,i,ch,curses.color_pair(1))
+        wrongLetter += 1
         i += 1
 
 curses.nocbreak()
